6dfae74043b2eb8ab34f37e6abbdfd1af95b8c58
kmkang <144207120+KMKang01@users.noreply.github.com>
2025-05-04 04:02:20 +0900

    [Feat] modify ClassDTO

--- src/main/java/com/ssanai/jumplearn/dto/mainpage/ClassDTO.java
+++ src/main/java/com/ssanai/jumplearn/dto/mainpage/ClassDTO.java
@@ -3,6 +3,8 @@ package com.ssanai.jumplearn.dto.mainpage;
 import lombok.*;
 import lombok.extern.log4j.Log4j2;
 
+import java.time.LocalDateTime;
+
 @Log4j2
 @Data
 @ToString
@@ -13,11 +15,15 @@ public class ClassDTO {
 	public String file_name;
 	public String file_path;
 	public String file_ext;
+	public long file_size;
 	public int id; // 강좌 id
+	public String category;
 	public String title; // 강좌 제목
 	public String introduce; // 강좌 소개
 	public String teacher_id; // 강사
 	public int price; // 가격
 	public String target; // 대상
 	public int pay_count; // 팔린 수
+	public LocalDateTime created_at;
+	public String notice;
 }
