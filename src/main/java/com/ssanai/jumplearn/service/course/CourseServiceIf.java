6c3f5984188df6e97cf76761327c6ed08c818039
kmkang <144207120+KMKang01@users.noreply.github.com>
2025-05-04 04:01:47 +0900

    [Feat] add CourseService

---
+++ src/main/java/com/ssanai/jumplearn/service/course/CourseServiceIf.java
@@ -0,0 +1,9 @@
+package com.ssanai.jumplearn.service.course;
+
+import com.ssanai.jumplearn.dto.mainpage.ClassDTO;
+
+import java.util.List;
+
+public interface CourseServiceIf {
+	public List<ClassDTO> getClassList();
+}
---
+++ src/main/java/com/ssanai/jumplearn/service/course/CourseServiceImpl.java
@@ -0,0 +1,25 @@
+package com.ssanai.jumplearn.service.course;
+
+import com.ssanai.jumplearn.dto.mainpage.ClassDTO;
+import com.ssanai.jumplearn.mapper.course.CourseMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Log4j2
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CourseServiceImpl implements CourseServiceIf{
+	private final CourseMapper courseMapper;
+	private final ModelMapper modelMapper;
+
+	@Override
+	public List<ClassDTO> getClassList() {
+		return courseMapper.getClassList();
+	}
+}
