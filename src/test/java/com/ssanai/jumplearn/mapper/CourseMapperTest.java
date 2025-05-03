6f2e240eeffa9e136ffb181b47b7204b3f683329
kmkang <144207120+KMKang01@users.noreply.github.com>
2025-05-04 03:58:39 +0900

    [Test] add CourseMapperTest

---
+++ src/test/java/com/ssanai/jumplearn/mapper/CourseMapperTest.java
@@ -0,0 +1,28 @@
+package com.ssanai.jumplearn.mapper;
+
+import com.ssanai.jumplearn.dto.mainpage.ClassDTO;
+import com.ssanai.jumplearn.mapper.course.CourseMapper;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+@Log4j2
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/root-context.xml")
+public class CourseMapperTest {
+	@Autowired(required = false)
+	CourseMapper courseMapper;
+
+	@Test
+	public void getClassListTest() {
+		List<ClassDTO> classList = courseMapper.getClassList();
+		for (ClassDTO classDTO : classList) {
+			log.info(classDTO);
+		}
+	}
+}
