324baa6d0dddcb0f91fbce0f6409ad3fe49f184f
kmkang <144207120+KMKang01@users.noreply.github.com>
2025-05-04 03:58:25 +0900

    [Test] add CourseServiceTest

---
+++ src/test/java/com/ssanai/jumplearn/service/CourseServiceTest.java
@@ -0,0 +1,30 @@
+package com.ssanai.jumplearn.service;
+
+import com.ssanai.jumplearn.dto.mainpage.ClassDTO;
+import com.ssanai.jumplearn.service.course.CourseServiceIf;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+@Log4j2
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/root-context.xml")
+public class CourseServiceTest {
+	@Autowired(required = false)
+	CourseServiceIf courseService;
+
+	@Test
+	public void getClassListTest() {
+		List<ClassDTO> list = courseService.getClassList();
+		for (ClassDTO classDTO : list) {
+			log.info(classDTO);
+		}
+	}
+
+
+}
