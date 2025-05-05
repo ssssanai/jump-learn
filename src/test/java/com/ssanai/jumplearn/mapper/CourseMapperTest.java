d53d41beb13da28ccb78158c4c74d031b3319865
kmkang <wowkmini15@gmail.com>
2025-05-06 04:37:09 +0900

    [Feat] add test

--- src/test/java/com/ssanai/jumplearn/mapper/CourseMapperTest.java
+++ src/test/java/com/ssanai/jumplearn/mapper/CourseMapperTest.java
@@ -1,6 +1,7 @@
 package com.ssanai.jumplearn.mapper;
 
 import com.ssanai.jumplearn.dto.BasketDTO;
+import com.ssanai.jumplearn.dto.course.SearchDTO;
 import com.ssanai.jumplearn.dto.mainpage.ClassDTO;
 import com.ssanai.jumplearn.mapper.course.CourseMapper;
 import lombok.extern.log4j.Log4j2;
@@ -21,16 +22,93 @@ public class CourseMapperTest {
 
 	@Test
 	public void getClassListTest() {
-		List<ClassDTO> classList = courseMapper.getClassList();
+		List<ClassDTO> classList = courseMapper.getClassList(new SearchDTO());
 		for (ClassDTO classDTO : classList) {
 			log.info(classDTO);
 		}
 	}
 
 	@Test
+	public void getSearchListTest1() {
+		List<ClassDTO> classList = courseMapper.getClassList(
+				SearchDTO.builder()
+						.search_word("강사")
+						.search_condition1("강사")
+						.search_condition2("고2")
+						.build()
+		);
+		for (ClassDTO classDTO : classList) {
+			log.info(classDTO);
+		}
+	}
+
+	@Test
+	public void getSearchListTest2() {
+		List<ClassDTO> classList = courseMapper.getClassList(
+				SearchDTO.builder()
+						.search_word("강사")
+						.search_condition1("강사")
+						.search_condition2("고2")
+						.sort_condition("recently")
+						.build()
+		);
+		for (ClassDTO classDTO : classList) {
+			log.info(classDTO);
+		}
+	}
+
+	@Test
+	public void getSearchListTest3() {
+		List<ClassDTO> classList = courseMapper.getClassList(
+				SearchDTO.builder()
+						.search_condition1("제목")
+						.sort_condition("higher_price")
+						.build()
+		);
+		for (ClassDTO classDTO : classList) {
+			log.info(classDTO);
+		}
+	}
+
+	@Test
+	public void getSearchListTest4() {
+		List<ClassDTO> classList = courseMapper.getClassList(
+				SearchDTO.builder()
+						.search_condition1("제목")
+						.sort_condition("lower_price")
+						.build()
+		);
+		for (ClassDTO classDTO : classList) {
+			log.info(classDTO);
+		}
+	}
+
+	@Test
+	public void getSearchListTest5() {
+		List<ClassDTO> classList = courseMapper.getClassList(
+				new SearchDTO(1, 10, 0, 10, null, null, null, null, null)
+		);
+
+		for (ClassDTO classDTO : classList) {
+			log.info(classDTO);
+		}
+	}
+
+	@Test
+	public void getListTotalCountTest() {
+		int total_count = courseMapper.getListTotalCount(
+				SearchDTO.builder()
+						.search_condition1("제목")
+						.sort_condition("lower_price")
+						.build()
+		);
+		log.info(total_count);
+	}
+
+	@Test
 	public void getBasketListTest() {
 		List<BasketDTO> basketList = courseMapper.getBasketList("member002");
-		for(BasketDTO basketVO : basketList){
+		for (BasketDTO basketVO : basketList) {
 			log.info(basketVO);
 		}
 	}
