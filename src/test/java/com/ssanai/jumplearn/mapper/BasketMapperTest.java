2d7aa38b1c1cfc27d96b922b95c5887805b7df60
kmkang <wowkmini15@gmail.com>
2025-05-06 06:31:49 +0900

    [Test] add Basket Mapper & Service Test

---
+++ src/test/java/com/ssanai/jumplearn/mapper/BasketMapperTest.java
@@ -0,0 +1,31 @@
+package com.ssanai.jumplearn.mapper;
+
+import com.ssanai.jumplearn.dto.BasketDTO;
+import com.ssanai.jumplearn.dto.mainpage.ClassDTO;
+import com.ssanai.jumplearn.mapper.basket.BasketMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+@Log4j2
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/root-context.xml")
+public class BasketMapperTest {
+	@Autowired(required = false)
+	BasketMapper basketMapper;
+
+	@Test
+	public void getBasketListTest() {
+		List<BasketDTO> basketList = basketMapper.getBasketList("member006");
+		for (BasketDTO basketVO : basketList) {
+			log.info(basketVO);
+		}
+	}
+}
---
+++ src/test/java/com/ssanai/jumplearn/service/BasketServiceTest.java
@@ -0,0 +1,28 @@
+package com.ssanai.jumplearn.service;
+
+import com.ssanai.jumplearn.dto.BasketDTO;
+import com.ssanai.jumplearn.service.basket.BasketServiceIf;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+@Log4j2
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/root-context.xml")
+public class BasketServiceTest {
+	@Autowired(required = false)
+	BasketServiceIf basketService;
+
+	@Test
+	public void getBasketListTest() {
+		List<BasketDTO> list = basketService.getBasketList("member006");
+		for (BasketDTO basketDTO : list) {
+			log.info(basketDTO);
+		}
+	}
+}
