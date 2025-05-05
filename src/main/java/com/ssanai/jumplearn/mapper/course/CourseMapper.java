421c3cf997bd8d447a0ec7ea5fe6bf76aececd36
kmkang <144207120+KMKang01@users.noreply.github.com>
2025-05-05 16:58:02 +0900

    [Feat] add CourseMapper

--- src/main/java/com/ssanai/jumplearn/mapper/course/CourseMapper.java
+++ src/main/java/com/ssanai/jumplearn/mapper/course/CourseMapper.java
@@ -1,5 +1,6 @@
 package com.ssanai.jumplearn.mapper.course;
 
+import com.ssanai.jumplearn.dto.BasketDTO;
 import com.ssanai.jumplearn.dto.mainpage.ClassDTO;
 import org.apache.ibatis.annotations.Param;
 
@@ -7,5 +8,6 @@ import java.util.List;
 
 public interface CourseMapper {
 	public List<ClassDTO> getClassList();
+	public List<BasketDTO> getBasketList(@Param("id") String id);
 //	public List<ClassDTO> getClassList(@Param("start") int start, @Param("limit") int limit);
 }
