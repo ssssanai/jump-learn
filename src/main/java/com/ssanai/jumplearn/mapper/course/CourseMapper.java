d84c8da7129f6096584153945c58e624fc4acbcb
kmkang <wowkmini15@gmail.com>
2025-05-06 06:28:37 +0900

    [Feat] add getClassDetail, getReviewList, remove getBasketList

--- src/main/java/com/ssanai/jumplearn/mapper/course/CourseMapper.java
+++ src/main/java/com/ssanai/jumplearn/mapper/course/CourseMapper.java
@@ -1,7 +1,7 @@
 package com.ssanai.jumplearn.mapper.course;
 
-import com.ssanai.jumplearn.dto.BasketDTO;
-import com.ssanai.jumplearn.dto.PageRequestDTO;
+import com.ssanai.jumplearn.dto.ClassDetailDTO;
+import com.ssanai.jumplearn.dto.ReviewDTO;
 import com.ssanai.jumplearn.dto.course.SearchDTO;
 import com.ssanai.jumplearn.dto.mainpage.ClassDTO;
 import org.apache.ibatis.annotations.Param;
@@ -11,5 +11,6 @@ import java.util.List;
 public interface CourseMapper {
 	public List<ClassDTO> getClassList(SearchDTO searchDTO); // 검색 조건 없는 경우
 	public int getListTotalCount(SearchDTO searchDTO);
-	public List<BasketDTO> getBasketList(@Param("id") String id);
+	public ClassDetailDTO getClassDetailById(@Param("id") int id);
+	public List<ReviewDTO> getReviewListById(@Param("id") int id);
 }
