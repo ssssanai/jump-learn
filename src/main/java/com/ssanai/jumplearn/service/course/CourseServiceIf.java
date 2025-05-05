e8a65055691d11957ab66379855978bdebdb6523
kmkang <144207120+KMKang01@users.noreply.github.com>
2025-05-05 16:57:40 +0900

    [Feat] add CourseServiceIf

--- src/main/java/com/ssanai/jumplearn/service/course/CourseServiceIf.java
+++ src/main/java/com/ssanai/jumplearn/service/course/CourseServiceIf.java
@@ -1,9 +1,12 @@
 package com.ssanai.jumplearn.service.course;
 
+import com.ssanai.jumplearn.dto.BasketDTO;
 import com.ssanai.jumplearn.dto.mainpage.ClassDTO;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface CourseServiceIf {
 	public List<ClassDTO> getClassList();
+	public List<BasketDTO> getBasketList(@Param("id") String id);
 }
