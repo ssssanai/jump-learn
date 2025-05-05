6273a53b71e710e9fc21d3d0ee86c66e066ea44e
kmkang <wowkmini15@gmail.com>
2025-05-06 04:38:32 +0900

    [Feat] add fields for paging

--- src/main/java/com/ssanai/jumplearn/dto/course/SearchDTO.java
+++ src/main/java/com/ssanai/jumplearn/dto/course/SearchDTO.java
@@ -1,7 +1,10 @@
 package com.ssanai.jumplearn.dto.course;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.*;
 import lombok.extern.log4j.Log4j2;
+import org.checkerframework.checker.index.qual.Positive;
 
 @Log4j2
 @Data
@@ -10,6 +13,27 @@ import lombok.extern.log4j.Log4j2;
 @AllArgsConstructor
 @Builder
 public class SearchDTO {
+	@Builder.Default
+	@Positive
+	@Min(value=1)
+	private int page_no=1;
+	@Builder.Default
+	@Positive
+	@Min(value=1)
+	private int page_size=10;
+	@Builder.Default
+	@PositiveOrZero
+	@Min(value=0)
+	private int page_skip_count=0;
+	@Builder.Default
+	@Positive
+	@Min(value=1)
+	private int page_block_size=10;
+
+	public int getPage_skip_count() {
+		return (this.page_no-1)*this.page_size;
+	}
+
 	private String search_word;
 	private String search_condition1;
 	private String search_condition2;
