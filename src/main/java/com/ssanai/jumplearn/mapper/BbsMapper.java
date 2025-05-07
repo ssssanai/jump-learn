fabe1c2ef1bd44fa9552e68fda2cd3e2287190c3
wlsidelw <109274960+wlsidelw@users.noreply.github.com>
2025-05-07 22:09:58 +0900

    [feat]BbsMapper
    getTotalCount, searchList 추가

--- src/main/java/com/ssanai/jumplearn/mapper/BbsMapper.java
+++ src/main/java/com/ssanai/jumplearn/mapper/BbsMapper.java
@@ -1,15 +1,18 @@
 package com.ssanai.jumplearn.mapper;
 
 import com.ssanai.jumplearn.dto.BbsDefaultDTO;
+import com.ssanai.jumplearn.dto.PageRequestDTO;
 import com.ssanai.jumplearn.vo.BbsDefaultVO;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface BbsMapper {
-    public List<BbsDefaultVO> listAll();
+    public int getTotalCount(PageRequestDTO pageDTO);
 
-    public List<BbsDefaultVO> list();
+    public List<BbsDefaultVO> listAll(PageRequestDTO pageDTO);
+
+    public BbsDefaultVO selectOne( int id);
 
     public int insert(BbsDefaultVO vo);
 
@@ -17,6 +20,6 @@ public interface BbsMapper {
 
     public int delete(int id);
 
-    public BbsDefaultVO selectOne( int id);
+    public List<BbsDefaultVO> searchList(PageRequestDTO pageDTO);
 
 }
