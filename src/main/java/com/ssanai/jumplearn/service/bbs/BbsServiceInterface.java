27b4a816296cf22f9a6c8d8b047af1421adf5923
wlsidelw <109274960+wlsidelw@users.noreply.github.com>
2025-05-07 22:18:52 +0900

    [feat] BbsServiceInterface
    코드 정렬, searchList, getTotalCount 추가

--- src/main/java/com/ssanai/jumplearn/service/bbs/BbsServiceInterface.java
+++ src/main/java/com/ssanai/jumplearn/service/bbs/BbsServiceInterface.java
@@ -1,15 +1,16 @@
 package com.ssanai.jumplearn.service.bbs;
 
 import com.ssanai.jumplearn.dto.BbsDefaultDTO;
+import com.ssanai.jumplearn.dto.PageRequestDTO;
+import com.ssanai.jumplearn.dto.PageResponseDTO;
 import com.ssanai.jumplearn.vo.BbsDefaultVO;
 
 import java.util.List;
 
 public interface BbsServiceInterface {
+    public int getTotalCount(PageRequestDTO requestDTO);
 
-    public List<BbsDefaultDTO> listAll();
-
-    public List<BbsDefaultDTO> list();
+    public List<BbsDefaultDTO> listAll(PageRequestDTO pageDTO);
 
     public int insert(BbsDefaultDTO dto);
 
@@ -18,5 +19,7 @@ public interface BbsServiceInterface {
     public int delete(int id);
 
     public BbsDefaultDTO selectOne( int id);
+
+    public PageResponseDTO<BbsDefaultDTO> searchList(PageRequestDTO pageDTO);
 
 }
