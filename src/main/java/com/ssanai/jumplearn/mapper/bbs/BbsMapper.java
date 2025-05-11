0dc5cf522682ab479ca72108c7114f1c1e7fe77d
wlsidelw <109274960+wlsidelw@users.noreply.github.com>
2025-05-12 04:39:12 +0900

    [chore] BbsMapper
    bbs 폴더 밑으로 경로 수정

---
+++ src/main/java/com/ssanai/jumplearn/mapper/bbs/BbsMapper.java
@@ -0,0 +1,31 @@
+package com.ssanai.jumplearn.mapper.bbs;
+
+
+import com.ssanai.jumplearn.dto.PageRequestDTO;
+import com.ssanai.jumplearn.vo.BbsDefaultVO;
+import com.ssanai.jumplearn.vo.BbsFileVO;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface BbsMapper {
+    public int getTotalCount(PageRequestDTO pageDTO);
+
+    public List<BbsDefaultVO> listAll(PageRequestDTO pageDTO);
+
+    public BbsDefaultVO selectOne( int id);
+
+    public int insert(BbsDefaultVO vo);
+
+    public int update(BbsDefaultVO vo);
+
+    public int delete(int id);
+
+    public List<BbsDefaultVO> searchList(PageRequestDTO pageDTO);
+
+    public List<BbsFileVO> attachedPic(@Param("id") int id);
+
+    public List<BbsFileVO> attachedPdf(@Param("id") int id);
+
+    public int viewCount(@Param("id") int id);
+}
