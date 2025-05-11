7c861b658d66f85936fef98d030b365e47062e1d
wlsidelw <109274960+wlsidelw@users.noreply.github.com>
2025-05-12 04:36:33 +0900

    [feat] EduFileMapper
    mapper for file upload to edu

---
+++ src/main/java/com/ssanai/jumplearn/mapper/bbs/EduFileMapper.java
@@ -0,0 +1,12 @@
+package com.ssanai.jumplearn.mapper.bbs;
+
+import com.ssanai.jumplearn.dto.BbsFileDTO;
+import com.ssanai.jumplearn.vo.BbsDefaultVO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Map;
+
+@Mapper
+public interface EduFileMapper {
+    public int eduFileUpload(Map<String,Integer> params);
+}
