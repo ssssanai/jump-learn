4f75c3315c8b81f9db586899408d751db255ebd0
wlsidelw <109274960+wlsidelw@users.noreply.github.com>
2025-05-12 04:42:19 +0900

    [feat] BbsFileMapper
    BbsFileMapper 인터페이스

---
+++ src/main/java/com/ssanai/jumplearn/mapper/bbs/BbsFileMapper.java
@@ -0,0 +1,9 @@
+package com.ssanai.jumplearn.mapper.bbs;
+
+import com.ssanai.jumplearn.dto.BbsFileDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface BbsFileMapper {
+    public int fileUpload(BbsFileDTO dto);
+}
